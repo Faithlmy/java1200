64248706668db0ce9fdcf9b6858fe4b7cf023367
lmy <lmengyy@126.com>
2016-11-05 23:46:46 +0800

    2346

--- Java1200/src/lmy1/InitAndclear.java
+++ Java1200/src/lmy1/InitAndclear.java
@@ -1,11 +1,19 @@
 /*
  * 初始化和清理：
  * 1、构造器与类同名
+ * 2、不接受任何参数的构造器叫做默认构造器
  * */
 
 package lmy1;
 
 public class InitAndclear {
+	public static void main(String[] args) {
+		for(int i = 0; i<10; i++)
+		{
+			//new Rock();
+			new Rock2(i);
+		}
+	}
 
 }
 //一个带有构造器的类
@@ -14,5 +22,13 @@ class Rock
 	Rock()
 	{
 		System.out.println("Rock");
+	}
+}
+//带有参数的构造器
+class Rock2
+{
+	Rock2(int i)
+	{
+		System.out.println("Rock2"+" " + i );
 	}
 }
