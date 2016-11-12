bdb0cdbf46339f8745a5a52d8741d526a11bc691
lmy <lmengyy@126.com>
2016-11-13 00:59:30 +0800

    mm

---
+++ Java1200/src/lmy5/Order.java
@@ -0,0 +1,22 @@
+package lmy5;
+
+public class Order {
+	private String orderName;
+	int orderId;
+	protected   int orderNum;
+	public String orderDesc;
+	
+	public void method1(){
+		
+	}
+	void method2(){
+		
+	}
+	protected void 	method3(){
+		
+	}
+	public void method4(){
+		 
+	}
+
+}
---
+++ Java1200/src/lmy5/OrderA.java
@@ -0,0 +1,5 @@
+package lmy5;
+
+public class OrderA {
+
+}
