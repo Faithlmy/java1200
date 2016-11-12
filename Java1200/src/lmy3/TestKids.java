b72e1437a2354fe29aa20e678e82f9ae2b5d1e26
lmy <lmengyy@126.com>
2016-11-12 23:58:08 +0800

    mm

--- Java1200/src/lmy3/TestKids.java
+++ Java1200/src/lmy3/TestKids.java
@@ -5,6 +5,8 @@ public class TestKids {
 	public static void main(String[] args) {
 		Kids  someKid = new Kids();
 		
+		
+		
 		someKid.setSalary(0);
 		someKid.setSex(1);
 		someKid.setYearsOld(13);
