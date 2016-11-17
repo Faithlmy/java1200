4c8ac32290e6aea37e035d5ab19817116bb0c3e0
lmy <lmengyy@126.com>
2016-11-17 23:25:19 +0800

    2

--- Java1200/src/lmy8/Testgiuhub2.java
+++ Java1200/src/lmy8/Testgiuhub2.java
@@ -2,7 +2,7 @@ package lmy8;
 
 public class Testgiuhub2 {
 	public static void main(String[] args) {
-		System.out.println("26");
+		System.out.println("2622");
 		System.out.println("22222");
 		
 	}
