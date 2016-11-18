b43bb987642c3f7f306ca6253c9ff91f41459996
lmy <lmengyy@126.com>
2016-11-18 23:15:39 +0800

    0

--- Java1200/src/lmy6/Testgithub.java
+++ Java1200/src/lmy6/Testgithub.java
@@ -3,7 +3,7 @@ package lmy6;
 public class Testgithub {
 	public static void main(String[] args) {
 		System.out.println("master");
-		System.out.println("6");
+		System.out.println("9");
 		System.out.println("uu");
 	}
 
